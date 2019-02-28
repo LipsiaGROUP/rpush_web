94bf94186506fdf07ae315fd669f4d53f4447eb2
Mada Aryakusumah <lokermada@gmail.com>
2019-02-28 17:31:58 +0700

    remove unused rspec

--- lib/rpush_web/engine.rb
+++ lib/rpush_web/engine.rb
@@ -7,8 +7,6 @@ module RpushWeb
     end
 
     config.generators do |g|
-      g.test_framework :rspec
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
       g.assets false
       g.helper false
     end
